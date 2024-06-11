2e8dd303d3cb626a58921952aeb1d031d0330570
tmestrera@gmail.com <tmestrera@Gmail.com>
2024-06-11 21:51:39 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/string_generator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.string_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Random String Generator'),
+      ),
+      body: Center(),
+    );
+  }
+}
